ef2d0c32ba3e1c9d3972a4a7142fe6664d3d3188
Lisa Raber <s8liraber@stud.uni-saarland.de>
2022-11-17 10:24:55 +0100

    changed reference for quantile normalization

--- normalization.py
+++ normalization.py
@@ -1,6 +1,3 @@
-import pandas as pd
-from plotting import density_plot
-
 import qnorm as qn
 
 
@@ -10,14 +7,18 @@ def mean_normalization(df):
 
     return norm_df
 
+
 def min_max_normalization(df):
     sample_list = df.columns.values.tolist()[2:]
     mm_df = (df[sample_list] - df[sample_list].min()) / (df[sample_list].max() - df[sample_list].min())
 
     return mm_df
 
+
 def quantile_normaliziation(df):
-    #sample_list = df.columns.values.tolist()[2:]
-    df_qn = qn.quantile_normalize(df, axis=1)
+    arr = df.median(axis=1)
+    df['Median'] = arr
+
+    df_qn = qn.quantile_normalize(df, target=df['Median'])
 
     return df_qn
