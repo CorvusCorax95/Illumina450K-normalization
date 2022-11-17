06817607f100e6793b09f1840a19d2b2b0f232cb
Lisa Raber <s8liraber@stud.uni-saarland.de>
2022-11-17 10:25:11 +0100

    plots with dark mode

--- plotting.py
+++ plotting.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 from prepare_data import log_data
 from prepare_data import make_dataframe
 
+
 # Kann ich beim plotten nur die "inneren" 95% nehmen um Ausreißer zu ignorieren?
 
-def density_plot(df, title):
-    df.plot.density(linewidth=1, figsize=(20, 10))
+def density_plot(df, title, x1, x2):
+    plt.style.use('dark_background')
+    df.plot.density(linewidth=1, figsize=(20, 10), xlim=(x1, x2))
+    plt.legend([])
     plt.title(title)
-    plt.show()
