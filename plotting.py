0ffa9cd39058ef29a05baa663f612e9bc475282a
Lisa Raber <s8liraber@stud.uni-saarland.de>
2023-06-07 18:47:42 +0200

    nicer plots

--- plotting.py
+++ plotting.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 
 import streamlit as st
 
+from streamlit_extras.chart_container import chart_container
+
 import normalization as norm
 import prepare_data as prep
+from type import DataType
 
 
-def _density_plot(df, title, x1, x2):
+def _density_plot(df, title):
 	"""Makes a density-plot from the Dataframe, a title and set boundaries
 	for the x axis"""
 	plt.style.use('dark_background')
-	df.plot.density(linewidth=1, figsize=(20, 10), xlim=(x1, x2))
+	df.plot.density(linewidth=1, figsize=(20, 10))
 	plt.legend([])
 	plt.title(title)
 
@@ -24,159 +26,105 @@ def _boxplot(df, title):
 	plt.title(title)
 
 
-def logged_plot():
-	from streamlit_extras import chart_container
+def _containerize_chart(df, name):
+	with chart_container(df):
+		st.write(name)
+		st.pyplot(_density_plot(df, name))
+
+
+def _switch(data_type, df_log_meth, df_log_unmeth):
+	if data_type == DataType.LOG:
+		return df_log_meth, df_log_unmeth, "Logged Plots"
+	elif data_type == DataType.MEAN:
+		df_norm_meth = norm.mean_normalization(df_log_meth)
+		df_norm_unmeth = norm.mean_normalization(df_log_unmeth)
+		title = "Mean Normalization"
+	elif data_type == DataType.MINMAX:
+		df_norm_meth = norm.min_max_normalization(df_log_meth)
+		df_norm_unmeth = norm.min_max_normalization(df_log_unmeth)
+		title = "Min-Max Normalization"
+	elif data_type == DataType.QN:
+		df_norm_meth = norm.quantile_normaliziation(df_log_meth)
+		df_norm_unmeth = norm.quantile_normaliziation(df_log_unmeth)
+		title = "Quantile Normalization"
+	return df_norm_meth, df_norm_unmeth, title
+
+
+def default_plots(data_type):
+	df_meth, df_unmeth = prep.log_data()
+	df_norm_meth, df_norm_unmeth, title = _switch(data_type, df_meth, df_unmeth)
 
 	col_left, col_right = st.columns(2)
-	df_log_meth, df_log_unmeth = prep.log_data()
 	with col_left:
-		st.pyplot(_density_plot(df_log_meth, "Logged plot - methylated", 5, 17))
+		_containerize_chart(df_norm_meth, str(title + " - methylated"))
 	with col_right:
-		st.pyplot(
-			_density_plot(df_log_unmeth, "Logged plot - unmethylated", 5, 17))
-	return df_log_meth, df_log_unmeth
+		_containerize_chart(df_norm_unmeth, str(title + " - unmethylated"))
+	return df_norm_meth, df_norm_unmeth
 
-def mean_normalized_plots(show_df):
-	st.header("Mean Normalization")
-	df_log_meth, df_log_unmeth = prep.log_data()
-	# MEAN NORMALIZED
-	col_left, col_right = st.columns(2)
-	# MIN MAX NORMALIZED
-	with col_left:
-		st.subheader("Methylated Values")
-		df_meannorm_meth = norm.mean_normalization(df_log_meth)
-		st.pyplot(
-			_density_plot(df_meannorm_meth, "Mean Normalized - methylated", -4,
-			              3))
-		if show_df:
-			st.write(df_meannorm_meth)
-	with col_right:
-		st.subheader("Unmethylated Values")
-		# MEAN NORMALIZED
-		df_meannorm_unmeth = norm.mean_normalization(df_log_unmeth)
-		st.pyplot(
-			_density_plot(df_meannorm_unmeth, "Mean Normalized - unmethylated",
-			              -4,
-			              3))
-		if show_df:
-			st.write(df_meannorm_unmeth)
-	return df_meannorm_meth, df_meannorm_unmeth
 
-def min_max_normalized_plots(show_df):
-	st.header("Min-Max Normalization")
-	df_log_meth, df_log_unmeth = prep.log_data()
-	col_left, col_right = st.columns(2)
-	# MIN MAX NORMALIZED
-	with col_left:
-		st.subheader("Methylated Values")
-		df_minmax_meth = norm.min_max_normalization(df_log_meth)
-		st.pyplot(
-			_density_plot(df_minmax_meth, "Min-Max Normalized - methylated",
-			              -0.2,
-			              1.25))
-		if show_df:
-			st.write(df_minmax_meth)
-	## MIN MAX NORMALIZED
-	with col_right:
-		st.subheader("Unmethylated Values")
-		df_minmax_unmeth = norm.min_max_normalization(df_log_unmeth)
-		st.pyplot(
-			_density_plot(df_minmax_unmeth, "Min-Max Normalized - unmethylated",
-			              -0.2, 1.25))
-		if show_df:
-			st.write(df_minmax_unmeth)
-	return df_minmax_meth, df_minmax_unmeth
-
-def quantile_normalized_plots(show_df):
+def quantile_normalized_plots():
 	st.header("Quantile Normalization")
 	df_log_meth, df_log_unmeth = prep.log_data()
 	# QUANTILE NORMALIZED
 	col_left, col_right = st.columns(2)
 	with col_left:
-		st.subheader("Methylated Values")
-		# last column as Median column
 		df_log_meth['Median'] = df_log_meth.median(axis=1)
 		reference_options = df_log_meth.columns.values.tolist()[2:]
 		reference_meth = st.selectbox('Which reference do you want to use?',
 		                              reference_options,
 		                              (len(reference_options) - 1), key=0)
 		df_qn_meth = norm.quantile_normaliziation(df_log_meth, reference_meth)
-		if show_df:
-			st.write(df_qn_meth)
-		st.pyplot(_density_plot(df_qn_meth,
-		                        "Quantile Normalized plot (Median) - methylated",
-		                        5,
-		                        17))
+		_containerize_chart(df_qn_meth, "Quantile Normalized plot - methylated")
 	with col_right:
-		st.subheader("Unmethylated Values")
-
 		## QUANTILE NORMALIZED
 		# last column as Median column
 		df_log_unmeth['Median'] = df_log_unmeth.median(axis=1)
 		reference_options = df_log_unmeth.columns.values.tolist()[2:]
 		reference_unmeth = st.selectbox('Which reference do you want to use?',
 		                                reference_options,
-		                                (len(reference_options) - 1), key=1)
+		                                (len(reference_options) - 1),
+		                                key="reference")
 		df_qn_unmeth = norm.quantile_normaliziation(df_log_unmeth,
 		                                            reference_unmeth)
-		if show_df:
-			st.write(df_qn_unmeth)
-		st.pyplot(_density_plot(df_qn_unmeth,
-		                        "Quantile Normalized plot (Median) - unmethylated",
-		                        5, 17))
+		_containerize_chart(df_qn_meth, "Quantile Normalized plot - "
+		                                "unmethylated")
+
 	return df_qn_meth, df_qn_unmeth
 
 
-@st.cache_data
-def everything_beta(beta, types, bmiq, boxplot, bmiq_norm, beta_plot):
+def everything_beta(boxplot):
 	df_log_meth, df_log_unmeth = prep.log_data()
 	df_beta = prep.beta_value(df_log_meth, df_log_unmeth, 100)
 	st.write("Beta Values (Methylation Values)")
 	df_beta_t1, df_beta_t2 = prep.split_types(df_beta)
-	if beta_plot:
-		st.write("Beta-Value Plots")
-		_beta_value_plots(beta, types, df_beta, df_beta_t1, df_beta_t2)
-	if bmiq_norm:
-		st.write("BMIQ Normalization")
-		_bmiq_plots(bmiq, boxplot, df_beta_t2)
+
+	_beta_value_plots(df_beta, df_beta_t1, df_beta_t2, boxplot)
+	st.write("BMIQ Normalization")
 
 	return df_beta
 
 
-def _beta_value_plots(beta, types, df_beta, df_beta_t1, df_beta_t2):
+def _beta_value_plots(df_beta, df_beta_t1, df_beta_t2, boxplot):
+	_containerize_chart(df_beta, "Beta Values")
+
 	col_left, col_right = st.columns(2)
-	if beta:
-		st.write(df_beta)
 	with col_left:
-		if types:
-			st.write(df_beta_t1)
-		st.pyplot(_density_plot(df_beta_t1, "Beta Values Type 1",
-		                        0.0, 0.15))
+		_containerize_chart(df_beta_t1, "Beta Values Type 1")
 	with col_right:
-		if types:
-			st.write(df_beta_t2)
-		st.pyplot(_density_plot(df_beta_t2, "Beta Values type 2",
-		                        0.0, 0.15))
+		_containerize_chart(df_beta_t2, "Beta Values Type 2")
 
-
-
-def _bmiq_plots(bmiq, boxplot, df_beta_t2):
 	df_bmiq = norm.bmiq()
-	if bmiq:
-		st.write(df_bmiq)
-	st.pyplot(_density_plot(df_bmiq, "BMIQ Values", -0.5, 2.25))
+	_containerize_chart(df_bmiq, "BMIQ Values")
 	if boxplot:
-		bmiq_boxplots(df_beta_t2, df_bmiq)
+		_boxplots(df_beta_t2, df_bmiq)
 
 
-def bmiq_boxplots(df_beta_t2, df_bmiq):
+def _boxplots(df_beta_t2, df_bmiq):
 	st.pyplot(_boxplot(df_beta_t2.loc[:, df_beta_t2.columns != 'type'],
 	                   "Beta Value Boxplot"))
-
 	st.pyplot(_boxplot(df_bmiq, "BMIQ Normalized Boxplot"))
 
 
 def plot_random_dataframe(df):
 	from st_aggrid import AgGrid
 	st.write(AgGrid(df))
-
--- ui.py
+++ ui.py
@@ -4,6 +4,10 @@ import streamlit as st
 
 import plotting as plot
 import user
+from streamlit_extras.dataframe_explorer import dataframe_explorer
+from streamlit_extras.chart_container import chart_container
+
+from type import DataType
 
 """ UI.py
 This class is dealing with the Streamlit Structure. Single Figures etc. are 
@@ -52,48 +56,39 @@ def make_header():
 def make_plots():
 	"""Cares about all the plots and Dataframe-Views"""
 	with st.sidebar:
-		b_download = st.checkbox("Do you want the possibility to download?")
+		b_download = st.checkbox("Do you want the possibility to download "
+		                         "everything at once? Clicking the provided "
+		                         "button will reload the site entirely.")
 		st.write("What normalizations do you want to see?")
 		b_mean_norm = st.checkbox('Mean Normalization')
 		b_qn_norm = st.checkbox('Quantile Normalization')
 		b_minmax_norm = st.checkbox('Min-Max Normalization')
 		b_bmiq_norm = st.checkbox('Beta-Mixture-Quantile Normalization')
-		st.info('Decide here if you want to see the Dataframes.')
-		show_beta_df = st.checkbox('Beta Values as Dataframe')
-		show_beta_plot = st.checkbox('Beta Values as Plot')
-		show_types_df = st.checkbox('Beta Values as Dataframe (Type sorted)')
-		show_bmiq_df = st.checkbox('BMIQ-Normalized Dataframe')
+		st.info('Decide here if you want to see the Additional Info.')
 		show_boxplot_df = st.checkbox('Boxplots for BMIQ')
-		show_qn_df = st.checkbox('Quantile Normalized Dataframe')
-		show_mean_df = st.checkbox('Mean Normalized Dataframe')
-		show_minmax_df = st.checkbox('Minmax Normalized Dataframe')
 
 	if st.button("Start Normalization", key="start"):
 		with st.spinner("Wait for Normalization..."):
 			# ORIGINAL DATA
-			df_meth, df_unmeth = plot.logged_plot()
+			df_meth, df_unmeth = plot.default_plots(DataType.LOG)
 			user.convert_df(df_meth, 'download/log-meth.csv')
 			user.convert_df(df_unmeth, 'download/log-unmeth.csv')
+
 			# NORMALIZATION
 			if b_mean_norm:
-				df_meth, df_unmeth = plot.mean_normalized_plots(
-					show_mean_df)
+				df_meth, df_unmeth = plot.default_plots(DataType.MEAN)
 				user.convert_df(df_meth, 'download/meannorm-meth.csv')
 				user.convert_df(df_unmeth, 'download/meannorm-unmeth.csv')
-			if b_qn_norm:
-				df_meth, df_unmeth = plot.quantile_normalized_plots(show_qn_df)
-				user.convert_df(df_meth, 'download/qnnorm-meth.csv')
-				user.convert_df(df_unmeth, 'download/qnnorm-unmeth.csv')
 			if b_minmax_norm:
-				df_meth, df_unmeth = plot.min_max_normalized_plots(
-					show_minmax_df)
+				df_meth, df_unmeth = plot.default_plots(DataType.MINMAX)
 				user.convert_df(df_meth, 'download/minmaxnorm-meth.csv')
 				user.convert_df(df_unmeth, 'download/minmaxnorm-unmeth.csv')
-			if show_beta_plot or b_bmiq_norm:
-				df = plot.everything_beta(show_beta_df, show_types_df,
-				                          show_bmiq_df,
-				                          show_boxplot_df,
-				                          b_bmiq_norm, show_beta_plot)
+			if b_qn_norm:
+				df_meth, df_unmeth = plot.quantile_normalized_plots()
+				user.convert_df(df_meth, 'download/qnnorm-meth.csv')
+				user.convert_df(df_unmeth, 'download/qnnorm-unmeth.csv')
+			if b_bmiq_norm:
+				df = plot.everything_beta(show_boxplot_df)
 				user.convert_df(df, 'download/bmiq.csv')
 
 		st.success("Normalization completed!")
